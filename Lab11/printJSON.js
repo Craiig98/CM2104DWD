aeebb1bc17c5e23ffc0e30c70048eafcf0952814
Craiig98 <craigleith98@gmail.com>
2018-03-05 16:21:30 +0000

    lab 11 almost complete lab just final one

---
+++ Lab11/printJSON.js
@@ -0,0 +1,30 @@
+$(function(){
+  //document ready
+  alert("document ready");
+
+  $('#searchform').submit(function(){
+    //get current value and add to items list
+    var searchterms = $("#searchterms").val();
+    //call our search youtube function
+    getResultsFromOMDB(searchterms);
+    return false;
+  });
+});
+
+function getResultsFromOMDB(searchterms) {
+  //call youtube API using ajax
+  //build url for the request
+  var url = "http://www.omdbapi.com/?apikey=c619ea91&s=" + searchterms;
+  //use jquery json shortcut
+  $.getJSON(url, function(jsondata) {
+    //handle the results
+    printJSON(jsondata);
+  });
+}
+
+function printJSON(jsondata) {
+  //prints the JSON to the screen
+  var normal = JSON.stringify(jsondata);
+  $("#resultsbox").append("<p>" + normal + "</p>");
+
+}
